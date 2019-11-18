a2a5738cddedb3d479b0171e3651d80f56fd94a3
Beastmastur64 <smurftracer51@gmail.com>
2019-11-18 13:50:14 +0200

    asks two numbers and then adds them together

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 const run = () => {
 
   let text = '0'
-  text = text + 1
+  text = text + 1   //uppg1=01
 
   let number = 0
 
@@ -11,12 +11,12 @@ const run = () => {
     number = number + 1
   }
 
-  let array = ['1', '2', '3', '4', '5', '6']
+  let array = ['1', '2', '3', '4', '5', '6'] //uppg2=1
   for(let i = 0; i<3; i++) {
     array.pop()
   }
 
-  console.log('GOOD BYE!')
+  console.log('GOOD BYE!') //uppg3= ['1', '2', '3']
 
 }
 
