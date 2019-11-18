29a7cef763216eb3fe3298db3587527f882affc0
nicklas <foo@hotmail.com>
2019-11-18 14:31:27 +0200

    yes

---
+++ src/answers.js
@@ -0,0 +1,11 @@
+1. What is the value of "text" after line 6 has been executed?
+
+     Value = 1
+
+2. What is the value of "number" when the program has reached line 14?
+
+     Value = 1
+
+3. What is the value of array when the program has reached line 19
+
+     Value = idk
